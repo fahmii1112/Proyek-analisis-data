535234c5915138c6e0a5f037615f47e60f03cb51
fahmi <89383614+fahmii1112@users.noreply.github.com>
2024-10-06 08:20:47 +0700

    Add files via upload

---
+++ dashboarddd.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+air_quality_df = ('air_quality.csv')
+
+st.header('Proyek analisis :sparkles:')
+
+st.subheader('kualitas udara')
+ 
+ 
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    air_quality_df["RAIN"],
+    air_quality_df["SO2"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+ 
+st.pyplot(fig)
